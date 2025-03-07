75e9a275eec2b5c1236a22bc93a84841d44839de
R.Gadah <gadahrejoice3@gmail.com>
2025-03-07 07:47:11 +0200

    captcha configuration

--- CollegeManagement-Django/college_management_system/settings.py
+++ CollegeManagement-Django/college_management_system/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6LeDuusqAAAAAB4cnAu-KMaizjYbSyvzYpnFpA7E'  # Consider using your secret key
+SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'  # Consider using your secret key
+
+RECAPTCHA_SITE_KEY = '6LeDuusqAAAAAEAcR-YsCyxUe7_3cY__K3-uTGxK'
+RECAPTCHA_SECRET_KEY = '6LeDuusqAAAAAB4cnAu-KMaizjYbSyvzYpnFpA7E'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- CollegeManagement-Django/main_app/views.py
+++ CollegeManagement-Django/main_app/views.py
@@ -37,7 +37,7 @@ def doLogin(request, **kwargs):
         #Google recaptcha
         captcha_token = request.POST.get('g-recaptcha-response')
         captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
+        captcha_key = "settings.RECAPTCHA_SECRET_KEY"
         data = {
             'secret': captcha_key,
             'response': captcha_token
