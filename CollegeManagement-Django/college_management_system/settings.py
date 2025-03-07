42912e2fc9959e237dbeb10b53ad409c35bee795
R.Gadah <gadahrejoice3@gmail.com>
2025-03-07 08:56:43 +0200

    captcha configuration3

--- CollegeManagement-Django/college_management_system/settings.py
+++ CollegeManagement-Django/college_management_system/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'  # Consider using your secret key
 
-RECAPTCHA_SITE_KEY = '6LeDuusqAAAAAEAcR-YsCyxUe7_3cY__K3-uTGxK'
+
 RECAPTCHA_SECRET_KEY = '6LeDuusqAAAAAB4cnAu-KMaizjYbSyvzYpnFpA7E'
 
 # SECURITY WARNING: don't run with debug turned on in production!
